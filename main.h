fffa85a5b02a90f3db2a09ec999a318834ae017b
sammiesuama <samuelisuama95@gmail.com>
2022-11-21 10:35:11 -0800

    _printf.c

--- main.h
+++ main.h
@@ -1,32 +1,19 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
- */
-typedef struct print
-{
-char *t;
-int (*f)(va_list);
-} print_t;
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+int print_number(int n);
+int dec_to_binary(int n);
+int _putstring(char *s);
+void _reverse(char *s);
+int rev_str(char *s);
+int rot13(char *point);
+int print_digit(int n);
+int _strlen(char *s);
 
-#endif  /* _MAIN_H */
+#endif
