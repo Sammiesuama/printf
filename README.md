14f6a7f4c34ea3a23a770af19aff7bf1da46fc01
Johnfavour <favourjohn851@gmail.com>
2022-11-22 22:02:32 -0800

    task

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Write a function that produces output according to a format.
+0X11. C- printf
---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf - Prints a string in a formatted way
+ * format: input
+ * Return: numder of characters
+ */
+int _printf(const char *format, ...);
+{
+
