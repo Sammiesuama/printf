faa6e6b658eae924d64dc72e1de691b1d6ff37d8
sammiesuama <samuelisuama95@gmail.com>
2022-11-23 05:11:38 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+
 /**
  * _printf - Prints a string in a formatted way
  * format: input
@@ -22,50 +23,50 @@ int _printf(const char *format, ...)
  */
 int _printf(const char *format, ...)
 {
-  int i = 0;
-  int count = 0;
-  int value = 0;
-  va_list args;
-  va_start(args, format);
-  int (*f)(va_list);
+int i = 0;
+int count = 0;
+int value = 0;
+va_list args;
+va_start(args, format);
+int (*f)(va_list);
 
-  /*Prevent parsing a null pointer*/
-  if (format == NULL)
-    return (-1);
+/*Prevent parsing a null pointer*/
+if (format == NULL)
+return (-1);
 
-  /*Print each character of string*/
-  while (format[i])
-    {
-      if (format[i] != '%')
-    {
-     value = write(1,&format[i],1);
-     count = count + value;
-      i++;
-      continue;
-    }
-      if (format[i] == '%')
-	{
-	  f = check_specifier(&format[i + 1]);
-	  if (f != NULL)
-	    {
-	      value = f(args);
-	      count = count + value;
-	      i = i + 2;
-	      continue;
-	    }
-	  if  (format[i + 1] == '\0')
-	    {
-	      break;
-	    }
-	  if (format[i + 1] != '\0')
-	    {
-	      value = write(1, &format[i + 1], 1);
-	      count = count + value;
-	      i = i + 2;
-	      continue;
-	    }
-	}
-    }
-  return (count);
+/*Print each character of string*/
+while (format[i])
+{
+if (format[i] != '%')
+{
+value = write(1, &format[i], 1);
+count = count + value;
+i++;
+continue;
+}
+if (format[i] == '%')
+{
+f = check_specifier(&format[i + 1]);
+if (f != NULL)
+{
+value = f(args);
+count = count + value;
+i = i + 2;
+continue;
+}
+if  (format[i + 1] == '\0')
+{
+break;
+}
+if (format[i + 1] != '\0')
+{
+value = write(1, &format[i + 1], 1);
+count = count + value;
+i = i + 2;
+continue;
+}
+}
+}
+return (count);
 }
 }
