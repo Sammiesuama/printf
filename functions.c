25d289c9e086bf468c32784e3c68fc40a5030c70
sammiesuama <samuelisuama95@gmail.com>
2022-11-24 22:51:18 -0800

    printf

--- functions.c
+++ functions.c
@@ -158,6 +158,7 @@ for (i = 1; i < 32; i++)
 {
 m /= 2;
 a[i] = (n / m) % 2;
+}
 for (i = 0, sum = 0, count = 0; i < 32; i++)
 {
 sum += a[i];
@@ -170,4 +171,4 @@ count++;
 }
 return (count);
 }
-}
+
