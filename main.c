2a6d316e4922329aad88ce4f890620dbd7001045
sammiesuama <samuelisuama95@gmail.com>
2022-11-24 17:30:24 -0800

    printf

--- main.c
+++ main.c
@@ -10,18 +10,18 @@
  */
 int main(void)
 {
-  int count_std = 0;
-  int count_hmd = 0;
+int count_std = 0;
+int count_hmd = 0;
   
-  write(1,"STANDARD:\n",10);
-  count_std = printf("My name starts with: %c\n", 'F');
-  printf("I printed %d characters\n", count_std);
+write(1,"STANDARD:\n",10);
+count_std = printf("My name starts with: %c\n", 'F');
+printf("I printed %d characters\n", count_std);
 
-  write(1,"_________________________\n",25);
+write(1,"_________________________\n",25);
 
-  write(1,"HOME_MADE:\n",11);
-  count_hmd = _printf("My name starts with: %c\n", 'F');
-  printf("I printed %d characters\n", count_hmd);
+write(1,"HOME_MADE:\n",11);
+count_hmd = _printf("My name starts with: %c\n", 'F');
+printf("I printed %d characters\n", count_hmd);
 
-  return (0);
+return (0);
 }
